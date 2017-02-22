9d94611c4494850a1828e6e639b01f4f1a1545af
Sergo8707 <sergo8707@gmail.com>
2017-02-22 19:10:22 +0200

    create test model question

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  context 'validation' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
+
+  context 'association' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+end
