0734437fe74dad021b54612ad4d5ce840a886f66
Sergo8707 <sergo8707@gmail.com>
2017-02-22 20:30:07 +0200

    create test model answer

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Answer do
+
+  context 'validation' do
+    it { should validate_presence_of :body }
+    it { should validate_presence_of :question }
+  end
+
+  context 'association' do
+    it { should belong_to(:question) }
+  end
+end
