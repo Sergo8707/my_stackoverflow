30eca3b9c5dfbaa63ba07f62e94f731cdb0d8863
Sergo8707 <sergo8707@gmail.com>
2017-02-26 22:43:11 +0200

    create model user and user_spec

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,24 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_223_222_219) do
+ActiveRecord::Schema.define(version: 20170226203518) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.text     'body',        null: false
-    t.integer  'question_id', null: false
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id', using: :btree
+  create_table "answers", force: :cascade do |t|
+    t.text     "body",        null: false
+    t.integer  "question_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string   'title',      null: false
-    t.text     'body',       null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "questions", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'answers', 'questions'
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "answers", "questions"
 end
