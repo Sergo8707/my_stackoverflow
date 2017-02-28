1cf344f261405856fb8af301638b2af2b952e731
Sergo8707 <sergo8707@gmail.com>
2017-02-28 10:41:13 +0200

    add acceptance tests delete_question and delete_question

---
+++ spec/acceptance/questions/view_questions_spec.rb
@@ -0,0 +1,24 @@
+feature 'View questions', %q{
+  In order to read the text of the question
+  As a user
+  I want to be able to see the issue
+} do
+  scenario 'User view questions' do
+    questions = create_list(:question, 5)
+    visit questions_path
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
+  end
+
+  scenario 'User view question and answers to it' do
+    question = create(:question)
+    answers = create_list(:answer, 5, question: question)
+    visit question_path(question)
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end
