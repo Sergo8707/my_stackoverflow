6eebe090013934dd5add31746b763dbb26db970e
Sergo8707 <sergo8707@gmail.com>
2017-02-28 10:19:24 +0200

    Add User Reference To Questions And Answers

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development do
 
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-rails'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  belongs_to :user
 
   validates :title, :body, presence: true
 end
