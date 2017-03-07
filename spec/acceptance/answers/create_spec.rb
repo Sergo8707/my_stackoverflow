84b63e2eb3b272c658e194d66e7eb69d7fa1baba
Sergo8707 <sergo8707@gmail.com>
2017-03-07 20:21:03 +0200

    Answer errors

--- spec/acceptance/answers/create_spec.rb
+++ spec/acceptance/answers/create_spec.rb
@@ -20,12 +20,24 @@ feature 'Create answer', '
     expect(current_path).to eq question_path(question)
   end
 
-  scenario 'Authenticated user create answer with invalid attributes', js: true do
-    sign_in(user)
-    visit question_path(question)
-    fill_in 'Текст ответа', with: 'text567'
-    click_on 'Ответить'
-    expect(page).not_to have_content 'text567'
+  describe 'Authenticated user create answer with invalid attributes', js: true do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'body text is too short', js: true do
+      fill_in 'Текст ответа', with: 'text567'
+      click_on 'Ответить'
+      expect(page).not_to have_content 'text567'
+      expect(page).to have_content 'Body слишком короткий'
+    end
+
+    scenario 'body text is blank', js: true do
+      fill_in 'Текст ответа', with: ''
+      click_on 'Ответить'
+      expect(page).to have_content "Body не может быть пустым!"
+    end
   end
 
   scenario 'Non-authenticated user try to creates qiestion', js: true do
