3e1fa6cd1b6fa68f6ba9ea6a902da610d467e390
Sergo8707 <sergo8707@gmail.com>
2017-03-13 12:48:02 +0200

    Attachment model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_many :attachments, dependent: :destroy
+
   belongs_to :user
 
   validates :title, :body, presence: true, length: { minimum: 10 }
