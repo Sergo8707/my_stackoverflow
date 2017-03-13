431d1aa87a57e48d347ad8c64b09d7cb66426cb0
Sergo8707 <sergo8707@gmail.com>
2017-03-13 12:40:45 +0200

    add carrierwave uploaders and tests

---
+++ spec/acceptance/questions/add_files_spec.rb
@@ -0,0 +1,22 @@
+feature 'Add files to question', %q{
+  In order to illustrate my question
+  As an question's author
+  I'd like to be able to attach files
+} do
+  given(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'User adds file when asks question' do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text question'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb'
+  end
+
+end
