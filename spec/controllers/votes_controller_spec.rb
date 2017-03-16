e4718e3b37520959f5c17b2df9837ee4230d22ad
Sergo8707 <sergo8707@gmail.com>
2017-03-16 18:07:15 +0200

    user can vote only once and votable url

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
   let(:question) { create(:question) }
-  let!(:vote_params) { { question_id: question, votable_type: question.class.name, rating: 'up', format: :json } }
+  let!(:vote_params) { {question_id: question, rating: 'up', format: :json} }
 
   describe 'POST #create' do
     context 'Authenticated user' do
@@ -28,45 +28,32 @@ RSpec.describe VotesController, type: :controller do
         end
       end
 
-      context 'with invalid attributes' do
-        let(:votable_type_missing) { { question_id: question, rating: 'up', format: :json } }
-        let(:invalid_votable_type) { { question_id: question, votable_type: 'Trulala', rating: 'up', format: :json } }
+      context 'double voting' do
+        before { create(:vote, votable: question, user: @user) }
 
-        context 'does not save the vote' do
-          it 'votable type missing' do
-            expect do
-              post :create, params: votable_type_missing
-            end.to_not change(Vote, :count)
-          end
-
-          it 'invalid votable type' do
-            expect do
-              post :create, params: invalid_votable_type
-            end.to_not change(Vote, :count)
-          end
+        it 'tries vote again' do
+          expect { post :create, params: vote_params }.to_not change(question.votes, :count)
         end
+      end
 
-        context 'renders error' do
-          it 'votable type missing' do
-            post :create, params: votable_type_missing
-            data = JSON.parse(response.body)
-            expect(response).to have_http_status :bad_request
-            expect(data['error']).to eq 'Error'
-            expect(data['error_message']).to eq 'Not the correct vote data!'
+      context 'with invalid attributes' do
+        let(:rating_missing) { {question_id: question, format: :json} }
+        let(:invalid_rating) { {question_id: question, rating: 'something', format: :json} }
+
+        context 'save vote with a negative evaluation' do
+          it 'rating missing' do
+            expect { post :create, params: rating_missing }.to change(question.votes, :count).by(1)
+            expect(question.rating).to eq(-1)
           end
 
-          it 'invalid votable type' do
-            post :create, params: invalid_votable_type
-            data = JSON.parse(response.body)
-            expect(response).to have_http_status :bad_request
-            expect(data['error']).to eq 'Error'
-            expect(data['error_message']).to eq 'Not the correct vote data!'
+          it 'invalid rating' do
+            expect { post :create, params: invalid_rating }.to change(question.votes, :count).by(1)
+            expect(question.rating).to eq(-1)
           end
         end
       end
 
       context 'User is author votable' do
-        let!(:vote_params) { { question_id: question, votable_type: question.class.name, rating: 'up', format: :json } }
         before { sign_in question.user }
 
         it 'vote not stored in the database' do
@@ -95,18 +82,17 @@ RSpec.describe VotesController, type: :controller do
   describe 'DELETE #destroy' do
     let(:user) { create(:user) }
     let(:question) { create(:question) }
-    # let!(:vote) { create(:vote_question, votable: question, user: user) }
     let!(:vote) { create(:vote, votable: question, user: user) }
 
     context 'Authenticated user' do
       before { sign_in vote.user }
       context 'author vote' do
         it 'delete vote' do
-          expect { delete :destroy, params: { id: vote.id, format: :json } }.to change(Vote, :count).by(-1)
+          expect { delete :destroy, params: {id: vote.id, format: :json} }.to change(Vote, :count).by(-1)
         end
 
         it 'render success' do
-          delete :destroy, params: { id: vote.id, format: :json }
+          delete :destroy, params: {id: vote.id, format: :json}
           question.reload
           data = JSON.parse(response.body)
           expect(response).to have_http_status :success
@@ -124,11 +110,11 @@ RSpec.describe VotesController, type: :controller do
         before { sign_in question.user }
 
         it 'delete vote' do
-          expect { delete :destroy, params: { id: vote.id, format: :json } }.to_not change(Vote, :count)
+          expect { delete :destroy, params: {id: vote.id, format: :json} }.to_not change(Vote, :count)
         end
 
         it 'render error' do
-          delete :destroy, params: { id: vote.id, format: :json }
+          delete :destroy, params: {id: vote.id, format: :json}
           data = JSON.parse(response.body)
           expect(response).to have_http_status :forbidden
           expect(data['error']).to eq 'Error remove'
@@ -139,7 +125,7 @@ RSpec.describe VotesController, type: :controller do
 
     context 'Non-authenticated user' do
       it 'delete vote' do
-        expect { delete :destroy, params: { id: vote.id, format: :json } }.to_not change(Vote, :count)
+        expect { delete :destroy, params: {id: vote.id, format: :json} }.to_not change(Vote, :count)
       end
     end
   end
