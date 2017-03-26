1a0f522b349a62f8d9188414e51383353dc4c3ed
Sergo8707 <sergo8707@gmail.com>
2017-03-26 12:14:36 +0300

    create vote presenter

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,7 +10,7 @@ class VotesController < ApplicationController
     if !current_user.author?(@context) && @context.vote_user(current_user).nil?
       @vote = @context.send("vote_#{vote_params[:rating]}", current_user)
       if @vote.persisted?
-        render_success(prepare_data(@vote), 'create', 'Your vote has been accepted!')
+        render json: VotePresenter.new(@vote).as(:create)
       else
         render_error(:unprocessable_entity, 'Error save', 'Not the correct vote data!')
       end
@@ -23,21 +23,13 @@ class VotesController < ApplicationController
     vote = Vote.find(params[:id])
     if current_user.author?(vote)
       vote.destroy
-      render_success(prepare_data(vote), 'delete', 'Your vote removed!')
+      render json: VotePresenter.new(vote).as(:destroy)
     else
       render_error(:forbidden, 'Error remove', 'You can not remove an vote!')
     end
   end
 
   private
-
-  def prepare_data(item)
-    item.slice(:id, :votable_id)
-        .merge(
-          votable_type: item.votable_type.underscore,
-          votable_rating: item.votable.rating
-        )
-  end
 
   def vote_params
     { rating: params[:rating] == 'up' ? :up : :down }
