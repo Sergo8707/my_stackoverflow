8e32015dd5711ac77c83c23c5e2e07ea0bf9592a
Sergo8707 <sergo8707@gmail.com>
2017-04-06 23:51:53 +0300

    api questions

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::BaseController < ApplicationController
-
   before_action :doorkeeper_authorize!
 
   respond_to :json
@@ -8,5 +7,9 @@ class Api::V1::BaseController < ApplicationController
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @ability ||= Ability.new(current_resource_owner)
   end
 end
